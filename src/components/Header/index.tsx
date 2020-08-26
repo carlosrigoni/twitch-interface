95b5533c6e30611fb3a02d4d4330448070132096
Carlos Daniel Rigoni <danrigonisocial@gmail.com>
2020-08-26 10:17:43 -0300

    header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { MaterialIcons, MaterialCommunityIcons, Feather } from '@expo/vector-icons';
+
+import { Container, Avatar, OnlineStatus, RightSide, Button } from './styles';
+
+import colors from '../../styles/colors';
+
+const Header: React.FC = () => {
+  return (
+    <Container>
+      <Avatar>
+        <OnlineStatus />
+      </Avatar>
+
+      <RightSide>
+        <Button>
+          <MaterialIcons
+            name="notifications-none"
+            size={26}
+            color={colors.black}
+          />
+        </Button>
+
+        <Button>
+          <MaterialCommunityIcons
+            name="message-outline"
+            size={26}
+            color={colors.black}
+          />
+        </Button>
+
+        <Button>
+          <Feather
+            name="search"
+            size={26}
+            color={colors.black}
+          />
+        </Button>
+      </RightSide>
+    </Container>
+  );
+};
+
+export default Header;
--- src/pages/Following/index.tsx
+++ src/pages/Following/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-import { Container } from './styles';
+import Header from '../../components/Header';
+
+import { Wrapper, Container, Main } from './styles';
 
 const Following: React.FC = () => {
   return (
-    <Container>
-      <Text>Following</Text>
-    </Container>
+    <Wrapper>
+      <Container>
+        <Header />
+        <Main />
+      </Container>
+    </Wrapper>
   );
 };
 
