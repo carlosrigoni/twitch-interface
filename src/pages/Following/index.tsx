ec98167909abe39857e545a04a4af35b93abf3ed
Carlos Daniel Rigoni <danrigonisocial@gmail.com>
2020-08-26 11:04:50 -0300

    componnents

--- src/pages/Following/index.tsx
+++ src/pages/Following/index.tsx
@@ -1,16 +1,86 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, FlatList } from 'react-native';
 
 import Header from '../../components/Header';
+import Heading from '../../components/Heading';
+import Title from '../../components/Title';
 
 import { Wrapper, Container, Main } from './styles';
 
+interface Item {
+  key: string;
+  render: () => JSX.Element;
+  isTitle?: boolean;
+}
+
 const Following: React.FC = () => {
+  const { data, indices } = React.useMemo(() => {
+    const items: Item[] = [
+      {
+        key: 'PAGE_HEADING',
+        render: () => <Heading>Following</Heading>
+      },
+
+      {
+        key: 'FOLLOWED_CATEGORIES',
+        render: () => <Title>Followed Categories</Title>,
+        isTitle: true,
+      },
+
+      { key: 'C1', render: () => <View /> },
+
+      {
+        key: 'LIVE_CHANNELS',
+        render: () => <Title>Live Channels</Title>,
+        isTitle: true,
+      },
+
+      { key: 'C2', render: () => <View /> },
+
+      {
+        key: 'CONTINUE_WATCHING',
+        render: () => <Title>Continue watching</Title>,
+        isTitle: true,
+      },
+
+      { key: 'C3', render: () => <View /> },
+
+      {
+        key: 'OFFLINE_CHANNELS',
+        render: () => <Title>Offline Channels</Title>,
+        isTitle: true,
+      },
+
+      { key: 'C4', render: () => <View /> },
+    ];
+
+    const indices: number[] = [];
+
+    items.forEach((item, index) => item.isTitle && indices.push(index));
+
+    return {
+      data: items,
+      indices: indices
+    }
+  }, [])
+
   return (
     <Wrapper>
       <Container>
         <Header />
-        <Main />
+
+        <Main >
+          <FlatList<Item>
+            data={data}
+            renderItem={({ item }) => item.render()}
+            keyExtractor={item => item.key}
+            stickyHeaderIndices={indices}
+
+            onRefresh={() => {}}
+            refreshing={false}
+
+          />
+        </Main>
       </Container>
     </Wrapper>
   );
